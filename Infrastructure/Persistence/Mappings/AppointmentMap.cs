667d3475285b6cce041830fc8cd9cdeb84055985
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-05-20 10:13:41 -0300

    feat: add infrastructure

---
+++ Infrastructure/Persistence/Mappings/AppointmentMap.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Mappings;
+
+public class AppointmentMap : IEntityTypeConfiguration<Appointment>
+{
+    public void Configure(EntityTypeBuilder<Appointment> builder)
+    {
+        builder.ToTable("Appointments");
+
+        // Chave primária
+        builder.HasKey(a => a.Id);
+
+        // Relacionamentos
+        builder.HasOne(a => a.Patient)
+            .WithMany()
+            .HasForeignKey(a => a.PatientId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(a => a.Doctor)
+            .WithMany()
+            .HasForeignKey(a => a.DoctorId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Configurações das propriedades
+        builder.Property(a => a.AppointmentDateTime)
+            .IsRequired()
+            .HasColumnType("datetime2");
+
+        builder.Property(a => a.Notes)
+            .HasMaxLength(1000)
+            .IsRequired(false);
+
+        builder.Property(a => a.Status)
+            .IsRequired()
+            .HasConversion(
+                v => v.ToString(),
+                v => (AppointmentStatus)Enum.Parse(typeof(AppointmentStatus), v))
+            .HasMaxLength(20);
+
+        // Índices
+        builder.HasIndex(a => a.PatientId);
+        builder.HasIndex(a => a.DoctorId);
+        builder.HasIndex(a => a.AppointmentDateTime);
+    }
+}
