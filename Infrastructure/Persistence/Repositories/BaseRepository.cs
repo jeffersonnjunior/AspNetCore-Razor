5436614368064299ec00a060f6baad8861a69c1f
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-05-20 15:23:38 -0300

    feat: add base repository

---
+++ Infrastructure/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,46 @@
+﻿using Infrastructure.Interfaces.IRepositories;
+using Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class BaseRepository : IBaseRepository
+{
+    private readonly AppDbContext _context;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public T Add<T>(T entity) where T : class
+    {
+        _context.Set<T>().Add(entity);
+        return entity;
+    }
+
+    public void Delete<T>(T entity) where T : class
+    {
+        _context.Set<T>().Remove(entity);
+    }
+
+    public T? Find<T>(Guid id) where T : class
+    {
+        return _context.Set<T>().Find(id);
+    }
+
+    public IQueryable<T> Query<T>() where T : class
+    {
+        return _context.Set<T>().AsQueryable();
+    }
+
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
+    }
+
+    public void Update<T>(T entity) where T : class
+    {
+        _context.Set<T>().Update(entity);
+    }
+}
