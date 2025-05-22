c3d45396f7f5a856377b55dc8894b4460e43be7d
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-05-22 14:23:38 -0300

    feat: add factory base

---
+++ Application/Factories/MedicalSpecialtyFactory.cs
@@ -0,0 +1,7 @@
+﻿using Application.Interfaces.IFactories;
+
+namespace Application.Factories;
+
+public class MedicalSpecialtyFactory : IMedicalSpecialtyFactory
+{
+}
---
+++ Application/Factories/OtherDocumentFactory.cs
@@ -0,0 +1,7 @@
+﻿using Application.Interfaces.IFactories;
+
+namespace Application.Factories;
+
+public class OtherDocumentFactory : IOtherDocumentFactory
+{
+}
