cc5f53137aa0b08c15651e7a558433516660674a
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-05-26 16:52:57 -0300

    feat: add docker-componse

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -3,6 +3,25 @@ using Infrastructure.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 IConfiguration configuration = builder.Configuration;
 
+bool isRunningInContainer = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    if (isRunningInContainer)
+    {
+
+        options.ListenAnyIP(5000);
+    }
+    else
+    {
+
+        options.ListenAnyIP(5001, listenOptions =>
+        {
+            listenOptions.UseHttps();
+        });
+    }
+});
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
