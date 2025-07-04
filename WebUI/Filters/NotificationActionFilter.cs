e4e7a27557006705a376c4c38f247f3809d5b85b
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-07-04 09:04:07 -0300

    feat: add filters

---
+++ WebUI/Filters/NotificationActionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.Json; // Adicione este using para serialização
+
+namespace WebUI.Filters;
+
+public class NotificationActionFilter : IActionFilter
+{
+    private readonly NotificationContext _notificationContext;
+
+    public NotificationActionFilter(NotificationContext notificationContext)
+    {
+        _notificationContext = notificationContext;
+    }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        //if (!context.HttpContext.Request.Headers.ContainsKey("Authorization")) context.Result = new UnauthorizedObjectResult("Autorização Ausente");
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        if (_notificationContext.HasNotifications())
+        {
+            var notifications = _notificationContext.GetNotifications();
+            Console.WriteLine("Notificações retornadas: " + JsonSerializer.Serialize(notifications));
+            context.Result = new BadRequestObjectResult(notifications);
+        }
+    }
+}
---
+++ WebUI/Filters/NotificationFilterConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WebUI.Filters;
+
+public static class NotificationFilterConfiguration
+{
+    public static IServiceCollection AddNotificationActionFilter(this IServiceCollection services)
+    {
+        services.AddMvc(options =>
+        {
+            options.Filters.Add<NotificationActionFilter>();
+        });
+
+        return services;
+    }
+}
