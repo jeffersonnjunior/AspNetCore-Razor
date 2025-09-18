aae9e13c1ae5afce659efa5cca8a8d48a92ff647
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-09-18 10:59:55 -0300

    feat: add razor pages

---
+++ WebUI/Pages/Pdfs/CreateVaccinePdf.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebUI.Pages.Pdfs
+{
+    public class CreateVaccinePdfModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
